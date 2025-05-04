36948a193748c283319e256fcf56779235219af0
Alec Chamberlain <aleccham@cisco.com>
2025-05-04 14:00:34 -0600

    Update bulk-install-java.sh

--- appdcli/bulk-install-java.sh
+++ appdcli/bulk-install-java.sh
@@ -1,1 +1,2 @@
 sudo ./appd install java -i inventory --auto-start -q ssh -m visits-service
+sudo ./appd configure smartagent --attach-configure-file lib/ld_preload.json  -i inventory -q ssh -m visits-service 

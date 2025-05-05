67213667ca806069323cb96b8c1a282ea88c37c3
Alec Chamberlain <aleccham@cisco.com>
2025-05-05 16:30:59 -0400

    Update bulk-install-java.sh

--- appdcli/bulk-install-java.sh
+++ appdcli/bulk-install-java.sh
@@ -1,2 +1,2 @@
 sudo ./appd install java -i inventory --auto-start -q ssh -m visits-service -a Petclinic-1 -n visits-service -t visits-service
-sudo ./appd configure smartagent --attach-configure-file /home/cisco/spring-petclinic-microservices/deployments/appdcli/lib/ld_preload.json -i inventory -q ssh -m visits-service --extra-vars "linux_config_path=/opt/appdynamics"
+sudo ./appd configure smartagent --attach-configure-file /home/cisco/spring-petclinic-microservices/deployments/appdcli/lib/ld_preload.json -i inventory -q ssh -m visits-service 

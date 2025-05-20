1ad29b64ac2495df51ad86a00dae64df7df589fb
Alec Chamberlain <aleccham@cisco.com>
2025-05-20 10:17:34 -0400

    Update bulk-install-ma.sh

--- appdcli/bulk-install-ma.sh
+++ appdcli/bulk-install-ma.sh
@@ -1,1 +1,1 @@
-sudo ./appd install machine -i inventory --auto-start -q ssh -m machineagent
+sudo ./appd install machine -i inventory --auto-start -q ssh -m machineagent --extra-vars "agent_version=25.1.0.4532"
